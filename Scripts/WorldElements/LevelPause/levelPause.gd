c9496e4e498404bd058b27dc9118ce156d757291
OutOfNet <neteoris@proton.me>
2025-03-09 11:40:55 +0000

    Bug fix.
    Fixed the bug where the cursor would display a pointing cursor upon hovering over the "resume" and "exit" buttons while the game isn't paused.

--- Scripts/WorldElements/LevelPause/levelPause.gd
+++ Scripts/WorldElements/LevelPause/levelPause.gd
@@ -12,6 +12,8 @@ func _process(_delta: float) -> void:
 		fruits.process_mode = Node.PROCESS_MODE_DISABLED
 		coins.process_mode = Node.PROCESS_MODE_DISABLED
 		checkpoints.process_mode = Node.PROCESS_MODE_DISABLED
+		pauseMenu.get_node(^"ResumeButton").set_default_cursor_shape(2)
+		pauseMenu.get_node(^"ExitButton").set_default_cursor_shape(2)
 		pauseMenu.modulate = Color(1, 1, 1, 1)
 		player.sprintDuration = 0
 		print("Game has been paused.")
@@ -20,6 +22,7 @@ func _process(_delta: float) -> void:
 		fruits.process_mode = Node.PROCESS_MODE_INHERIT
 		coins.process_mode = Node.PROCESS_MODE_INHERIT
 		checkpoints.process_mode = Node.PROCESS_MODE_INHERIT
-		pauseMenu.get_node(^"ResumeButton").set_cursor_shape(0)
+		pauseMenu.get_node(^"ResumeButton").set_default_cursor_shape(0)
+		pauseMenu.get_node(^"ExitButton").set_default_cursor_shape(0)
 		pauseMenu.modulate = Color(1, 1, 1, 0)
 		print("Game has been unpaused.")
--- Scripts/WorldElements/LevelPause/resumeButton.gd
+++ Scripts/WorldElements/LevelPause/resumeButton.gd
@@ -8,7 +8,7 @@ extends Button
 
 func _input(_event: InputEvent) -> void:
 	if Input.is_action_just_pressed("PauseOrExit") && pauseMenu.modulate.a == 0:
-		$".".grab_focus()
+		grab_focus()
 
 func _on_resumeButton_up() -> void:
 	if pauseMenu.modulate.a > 0:
@@ -16,4 +16,6 @@ func _on_resumeButton_up() -> void:
 		fruits.process_mode = Node.PROCESS_MODE_INHERIT
 		coins.process_mode = Node.PROCESS_MODE_INHERIT
 		checkpoints.process_mode = Node.PROCESS_MODE_INHERIT
+		pauseMenu.get_node(^"ResumeButton").set_default_cursor_shape(0)
+		pauseMenu.get_node(^"ExitButton").set_default_cursor_shape(0)
 		pauseMenu.modulate = Color(1, 1, 1, 0)
